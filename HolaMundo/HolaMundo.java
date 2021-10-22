a2a2fd196d8251a3d33885cae97ad1e4b18b8863
mendosan <smendozalopez@gmail.com>
2021-10-22 11:45:08 -0600

    Segundo cambio

--- HolaMundo/HolaMundo.java
+++ HolaMundo/HolaMundo.java
@@ -2,6 +2,6 @@ package HolaMundo;
 
 public class HolaMundo {
     public static void main(String[] args) {
-        System.out.println("Hola mundo cruel y despiadado");
+        System.out.println("Hola mundo cruel y despiadado\n");
     }
 }
